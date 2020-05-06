0ba1bafba7456fa22bf58b2bdc13cb77d2927ec3
Oz Ramos <thepersonaldev@gmail.com>
2020-05-05 19:54:10 -0700

    #3 Helper for getting caret position

--- src/ContenteditableCheckbox.js
+++ src/ContenteditableCheckbox.js
@@ -8,7 +8,7 @@ export default class ContenteditableCheckbox {
       editable: document.createElement('div'),
       input: document.createElement('input')
     }
-    
+
     // Create a wrapping container
     $el.appendChild(this.$.group)
     this.$.group.classList.add('contenteditable-checkboxes-group')
@@ -39,8 +39,8 @@ export default class ContenteditableCheckbox {
       }
 
       // Delete checkbox
-      if (ev.key === 'Backspace') {
-        console.log('yo')
+      if (ev.key === 'Backspace' && !this.getCaret()) {
+        console.log('delete')
       }
     })
   }
@@ -57,5 +57,35 @@ export default class ContenteditableCheckbox {
     this.$.group.classList.add('contenteditable-checkboxes-has-checkbox')
 
     this.$.editable.textContent = this.$.editable.textContent.substring(3)
+  }
+
+  /**
+   * Gets the caret position within the contenteditable
+   * @see https://stackoverflow.com/a/4812022
+   */
+  getCaret () {
+    let caretOffset = 0
+    let doc = this.$.editable.ownerDocument || this.$.editable.document
+    let win = doc.defaultView || doc.parentWindow
+    let sel
+
+    if (typeof win.getSelection != 'undefined') {
+      sel = win.getSelection()
+      if (sel.rangeCount > 0) {
+        let range = win.getSelection().getRangeAt(0)
+        let preCaretRange = range.cloneRange()
+        preCaretRange.selectNodeContents(this.$.editable)
+        preCaretRange.setEnd(range.endContainer, range.endOffset)
+        caretOffset = preCaretRange.toString().length
+      }
+    } else if ( (sel = doc.selection) && sel.type != 'Control') {
+      let textRange = sel.createRange()
+      let preCaretTextRange = doc.body.createTextRange()
+      preCaretTextRange.moveToElementText(this.$.editable)
+      preCaretTextRange.setEndPoint('EndToEnd', textRange)
+      caretOffset = preCaretTextRange.text.length
+    }
+    
+    return caretOffset
   }
 }
